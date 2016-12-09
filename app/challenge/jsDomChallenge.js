16aef31205b490c6f0e47e6b52ffce3b0175d905
Emanuele Longo <emanuele.longo@ynap.com>
2016-12-09 17:54:19 +0100

    fix

--- app/challenge/jsDomChallenge.js
+++ app/challenge/jsDomChallenge.js
@@ -1,7 +1,7 @@
 let inject = require('connect-inject');
 let IpToken = require('./ipToken');
 
-let span = '<span id="__id__" data-token="__data__"></span>';
+let span = '<span id="abcdef" data-token="__data__"></span>';
 
 let ch = 'function ch(id){elem = document.getElementById(id); console.log(elem.dataset.token);}';
 
@@ -19,9 +19,8 @@ class JsDomChallenge {
         let _this = this;
         return function(req, res, next) {
             let token = new IpToken(_this._getIp(req));
-            let spanId = Math.floor(Math.random() * 1000);
-            span = span.replace('__id__', spanId).replace('__data__', token.value);
-            inject({snippet: [span, '<script>(' + ch + ')(' + spanId + ')</script>']})(req, res, next);
+            span = span.replace('__data__', token.value);
+            inject({snippet: [span, '<script>(' + ch + ')(\"abcdef\")</script>']})(req, res, next);
         }
     }
 
